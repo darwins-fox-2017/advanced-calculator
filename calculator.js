7aa6f8d661483dd0d7dd65be8822e5ba4fb93d16
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-01 22:15:53 +0700

    Done all release

--- calculator.js
+++ calculator.js
@@ -2,7 +2,54 @@
 
 export class Calculator {
   //write your code here
+  constructor(param1){
+    this.p1 = param1
+    this.pi = 22/7
+  }
+
+  add(x){
+    this.p1 += x
+    return this
+  }
+
+  subtract(x){
+    this.p1 -= x
+    return this
+  }
+
+  multiply(x){
+    this.p1 *= x
+    return this
+  }
+
+  devide(x){
+    this.p1 /= x
+    return this
+  }
+
+  pow(x){
+    this.p1 = Math.pow(this.p1,x)
+    return this
+  }
+
+  sqrt(){
+    this.p1 = Math.pow(this.p1,0.5)
+    return this
+  }
+
+  areaOfCircle(){
+    this.p1 = this.pi*this.p1*this.p1
+    return this
+  }
+
+  print(){
+    console.log(this.p1)
+    return this
+  }
+
 }
+
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+
+let hitung = new Calculator(10)
+
+hitung.add(2).subtract(3).multiply(2).devide(2).pow(2).sqrt().areaOfCircle().print()
