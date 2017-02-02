da90e8c85a7299ba22c523b3ffcaddffbf883de3
Eri Irawan <eryirawan91@gmail.com>
2017-02-02 08:00:15 +0700

    Initial commit

--- calculator.js
+++ calculator.js
@@ -1,7 +1,39 @@
 "use strict";
 
 export class Calculator {
-  //write your code here
+  constructot(input){
+    this.input = input
+  }
+
+  calTambah(val){
+    this.input += val
+    return this
+  }
+
+  calKurang (val){
+    this.input -= val
+    return this
+  }
+
+  calBagi(val){
+    this.input  /= val
+    return this
+  }
+
+  calPangkat(val){
+    this.input = Math.pow(this.input, val)
+    return this
+  }
+
+  calKonstanta(val){
+    this.input = Math.pow(this.input, 2) * 3.14
+    return this
+  }
+
+  calResult(){
+    return `Result : ${this.input}`
+  }
+
 }
 
 /** note : you can use several features from ecmascript, such as:
