efaefd151dcbd378628a76d55805fc5b841282e1
danilags <aguspermadi94@gmail.com>
2017-02-03 20:09:04 +0700

    Daniel Advanced Calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,56 @@
 "use strict";
 
 export class Calculator {
-  //write your code here
+  constructor(nilai) {
+    this.nilai = nilai
+    this.PI = 3.14
+  }
+
+  penjumlahan(nilai) {
+    this.nilai += nilai
+    return this
+  }
+
+  pengurangan(nilai) {
+    this.nilai -= nilai
+    return this
+  }
+
+  perkalian(nilai) {
+    this.nilai *= nilai
+    return this
+  }
+
+  pembagian(nilai) {
+    this.nilai /= nilai
+    return this
+  }
+
+  pangkat(nilai) {
+    this.nilai = Math.pow(this.nilai, nilai)
+    return this
+  }
+
+  akar_pangkat(nilai) {
+    this.nilai = Math.sqrt(this.nilai)
+    return this
+  }
+
+  jari_jari_lingkaran(nilai) {
+    // PI r kuadrat
+    this.nilai = this.PI * Math.pow(this.nilai, 2)
+    return this
+  }
+
+  display() {
+    console.log(this.nilai);
+    return this
+  }
+
 }
 
+
+// let counter = new Calculator();
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+
+let kalkulator = new Calculator(50)
+kalkulator.penjumlahan(50).pengurangan(50).perkalian(2).pembagian(10).pangkat(2).akar_pangkat().jari_jari_lingkaran().display()
