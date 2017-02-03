9e9344f042e386eb63b3861e09adba3931579149
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-02-03 20:39:40 +0700

    advanced-calculator

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let calculatorHasil = new Calculator(20)
+calculatorHasil
+.Ditambah(10)
+.Dikali(2)
+.Dibagi(2)
+.Dikurang(5)
+.Perpangkatan(2)
+.Akar()
+.Lingkaran()
+.hasil()
