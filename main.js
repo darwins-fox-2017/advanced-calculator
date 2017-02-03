a408bcd19af962d24ffa906e0a6daf99b0bd1ae4
Diky Arga <dikyarga.id@gmail.com>
2017-02-03 21:39:49 +0700

    ES6 is Awesome!

--- main.js
+++ main.js
@@ -2,4 +2,6 @@
 
 import {Calculator} from "./calculator.js";
 
+let compute1 = new Calculator(2)
+console.log(compute1.sum(3).div(2).sub(1).power(8).squareRoot().display());
 // execute function on calculator.js in here
