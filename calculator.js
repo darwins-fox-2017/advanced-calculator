5975621361147b5d54f07179487a7a08b909ca95
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-04 14:26:13 +0700

    Advanced calculator

--- calculator.js
+++ calculator.js
@@ -2,6 +2,49 @@
 
 export class Calculator {
   //write your code here
+  constructor(num = 0) {
+    this.num = num
+    this.pi = Math.PI
+  }
+
+  penjumlahan(newNum) {
+    this.num += newNum
+    return this
+  }
+
+  pengurangan(newNum) {
+    this.num -= newNum
+    return this
+  }
+
+  perkalian(newNum) {
+    this.num *= newNum
+    return this
+  }
+
+  pembagian(newNum) {
+    this.num /= newNum
+    return this
+  }
+
+  pangkat(exponent) {
+    this.num = Math.pow(this.num,exponent)
+    return this
+  }
+
+  akarPangkat() {
+    this.num = Math.sqrt(this.num)
+    return this
+  }
+
+  luasLingkaran() {
+    this.num = (this.num*this.num)*this.pi
+    return this
+  }
+
+  samaDengan() {
+    return this.num
+  }
 }
 
 /** note : you can use several features from ecmascript, such as:
--- main.js
+++ main.js
@@ -3,3 +3,16 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let calculator = new Calculator()
+
+let count = calculator
+            .penjumlahan(2)
+            .penjumlahan(5)
+            .perkalian(10)
+            .pembagian(7)
+            .pengurangan(1)
+            .pangkat(3)
+            .akarPangkat()
+            .luasLingkaran()
+            .samaDengan()
+console.log(count);
