d90d8e66f7546300876a1b8b9cf5685e3938aee2
Bambang Handoko <email": null>
2017-02-04 19:46:16 +0700

    Calculator Done

--- calculator.js
+++ calculator.js
@@ -1,7 +1,50 @@
 "use strict";
 
 export class Calculator {
-  //write your code here
+  constructor(input){
+    this.input = input
+  }
+
+  addition(countNumber){
+    this.input += countNumber
+    return this
+  }
+
+  substraction(countNumber){
+    this.input -= countNumber
+    return this
+  }
+
+  multiply(countNumber){
+    this.input *= countNumber
+    return this
+  }
+
+  devide(countNumber){
+    this.input  /= countNumber
+    return this
+  }
+
+  power(countNumber){
+    this.input = Math.pow(this.input, countNumber)
+    return this
+  }
+
+  square(){
+    this.input = Math.sqrt(this.input)
+    return this
+  }
+
+  circle(){
+    const PI = 3.14
+    this.input = PI * Math.pow(this.input, 2)
+    return this
+  }
+
+  Result(){
+    console.log(`Result : ${this.input}`)
+    return this
+  }
 }
 
 /** note : you can use several features from ecmascript, such as:
