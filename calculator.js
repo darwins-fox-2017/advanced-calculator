ba58a19c135878a103751001a101b15ac32875e6
adamsaparudin <adamsaparudin@gmail.com>
2017-02-11 13:55:34 +0700

    SUDAH SELESAI

--- calculator.js
+++ calculator.js
@@ -1,8 +1,52 @@
 "use strict";
 
-export class Calculator {
+class Calculator {
   //write your code here
+  constructor(input){
+    this.input = input
+  }
+
+  tambah(val){
+    this.input += val
+    return this
+  }
+
+  kurang (val){
+    this.input -= val
+    return this
+  }
+
+  kali(val){
+    this.input *= val
+    return this
+  }
+
+  bagi(val){
+    this.input  /= val
+    return this
+  }
+
+  pangkat(val){
+    this.input = Math.pow(this.input, val)
+    return this
+  }
+
+  akar(){
+    this.input = Math.sqrt(this.input, 0.5)
+    return this
+  }
+
+  lingkaran(){
+    this.input = Math.pow(this.input, 2) * 22/7
+    return this
+  }
+
+  result(){
+    console.log(`Result : ${this.input}`)
+  }
+
 }
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
@@ -11,3 +55,7 @@ export class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+
+let hitung = new Calculator(5)
+
+hitung.pangkat(2).kali(10).result()
